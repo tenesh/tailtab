f759d1961d2194426370359c86684354c411db9a
tenesh <teneshdev@gmail.com>
2024-06-04 00:56:05 +0800

    wip

--- resources/js/components/public/NavigationBar.tsx
+++ resources/js/components/public/NavigationBar.tsx
@@ -1,9 +1,7 @@
-import { usePage } from '@inertiajs/react';
+import { usePage, Link } from '@inertiajs/react';
 import { PageProps } from '@/types';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { motion } from 'framer-motion';
-import { useRef } from 'react';
-import useClickOutside from '@/hooks/useClickOutside';
 
 interface NavigationBarProps {
     isOpen: boolean;
@@ -13,94 +11,95 @@ interface NavigationBarProps {
 export default function NavigationBar(props: NavigationBarProps) {
     const { appName, auth } = usePage<PageProps>().props;
 
-    const mobileMenuRef = useRef<HTMLDivElement>(null);
-
-    useClickOutside(mobileMenuRef, () => {
-        if (props.isOpen) {
-            props.onClose();
-        }
-    });
-
     return (
         <div className="flex items-center justify-between w-full max-w-[85rem] mx-auto text-secondary-900 p-4">
             <p className="text-secondary-900 text-3xl">{appName}</p>
-            {props.isOpen ? (
-                <motion.button
-                    initial="hide"
-                    animate={props.isOpen ? 'show' : 'hide'}
-                    variants={{
-                        show: { opacity: 1 },
-                        hide: { opacity: 0 },
-                    }}
-                    className="lg:hidden"
-                    onClick={props.onClose}
-                >
-                    <XMarkIcon className="w-8 text-secondary-900" />
-                </motion.button>
-            ) : (
-                <motion.button
-                    initial="show"
-                    animate={props.isOpen ? 'hide' : 'show'}
-                    variants={{
-                        show: { opacity: 1 },
-                        hide: { opacity: 0 },
-                    }}
-                    className="lg:hidden"
-                    onClick={props.onClose}
-                >
-                    <Bars3Icon className="w-8 text-secondary-900" />
-                </motion.button>
-            )}
             <nav className="hidden lg:flex items-center space-x-8">
-                <ul className="flex space-x-8">
-                    <li className="cursor-pointer">Pricing</li>
-                </ul>
+                <Link href="/features" className="hover:text-primary-600">
+                    Features
+                </Link>
+                <Link href="/pricing" className="hover:text-primary-600">
+                    Pricing
+                </Link>
+                <Link href="/resources" className="hover:text-primary-600">
+                    Resources
+                </Link>
                 {!auth.user && (
-                    <a
+                    <Link
                         href="/register"
-                        className="bg-primary-500 rounded-xl text-white px-4 py-2 hover:bg-secondary-400"
+                        className="bg-primary-500 rounded-xl text-white px-4 py-2 hover:bg-primary-600"
                     >
                         Register
-                    </a>
+                    </Link>
                 )}
                 {auth.user && (
-                    <a
+                    <Link
                         href="/dashboard"
-                        className="bg-primary-500 rounded-xl text-white px-4 py-2 hover:bg-secondary-400"
+                        className="bg-primary-500 rounded-xl text-white px-4 py-2 hover:bg-primary-600"
                     >
                         Dashboard
-                    </a>
+                    </Link>
                 )}
             </nav>
-            <motion.nav
-                initial="closed"
-                animate={props.isOpen ? 'open' : 'closed'}
-                variants={{
-                    open: { opacity: 1, width: 220 },
-                    closed: { opacity: 0, width: 0 },
-                }}
-                className="absolute top-0 left-0 max-w-[220px] h-full py-3 bg-secondary-50 lg:hidden"
-                ref={mobileMenuRef}
-            >
-                <div className="flex justify-between w-full text-secondary-900 p-4">
-                    <p className="text-secondary-900 text-3xl">{appName}</p>
-                </div>
-                <ul className="flex flex-col space-y-2 m-5">
-                    <a href="/pricing" className="cursor-pointer">
-                        Pricing
-                    </a>
-                    {!auth.user && (
-                        <a href="/register" className="cursor-pointer">
-                            Register
-                        </a>
-                    )}
-                    {auth.user && (
-                        <a href="/dashboard" className="cursor-pointer">
-                            Dashboard
-                        </a>
-                    )}
-                </ul>
-            </motion.nav>
+            <div className="lg:hidden">
+                <button className="lg:hidden" onClick={props.onClose}>
+                    <Bars3Icon className="w-8 text-secondary-900" />
+                </button>
+                <motion.div
+                    initial="closed"
+                    animate={props.isOpen ? 'open' : 'closed'}
+                    variants={{
+                        open: { opacity: 1, width: '100%' },
+                        closed: { opacity: 0, width: 0 },
+                    }}
+                    className="absolute top-0 left-0 w-full h-full py-3 bg-secondary-50 lg:hidden"
+                >
+                    <div className="flex justify-between w-full text-secondary-900 p-4">
+                        <p className="text-secondary-900 text-3xl">{appName}</p>
+                        <button className="lg:hidden" onClick={props.onClose}>
+                            <XMarkIcon className="w-8 text-secondary-900" />
+                        </button>
+                    </div>
+                    <nav className="flex flex-col w-full justify-center space-y-4 p-4 text-xl">
+                        <Link
+                            href="/features"
+                            className="hover:text-primary-500"
+                        >
+                            Features
+                        </Link>
+                        <Link
+                            href="/pricing"
+                            className="hover:text-primary-500"
+                        >
+                            Pricing
+                        </Link>
+                        <Link
+                            href="/resources"
+                            className="hover:text-primary-500"
+                        >
+                            Resources
+                        </Link>
+                    </nav>
+                    <div className="flex flex-col justify-between w-full text-secondary-900 p-4 text-xl">
+                        {!auth.user && (
+                            <Link
+                                href="/register"
+                                className="bg-primary-500 rounded-xl text-white text-center px-4 py-2 hover:bg-primary-600"
+                            >
+                                Register
+                            </Link>
+                        )}
+                        {auth.user && (
+                            <Link
+                                href="/dashboard"
+                                className="bg-primary-500 rounded-xl text-white text-center px-4 py-2 hover:bg-primary-600"
+                            >
+                                Dashboard
+                            </Link>
+                        )}
+                    </div>
+                </motion.div>
+            </div>
         </div>
     );
 }
